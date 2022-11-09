4617e6a0305ef97c3ddc8e4b011f0e88de724143
Gunjan Kuthe <gkuthe1997@gmail.com>
2022-11-09 23:58:08 +0530

    added day-2

--- credbee/src/App.js
+++ credbee/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Admin from './Components/Admin/Admin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Admin/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
