35d39689272c06093dcb65273b84607730135499
shubham020719 <101393523+shubham020719@users.noreply.github.com>
2022-11-09 21:07:51 +0530

    Day 1

---
+++ credbee/src/Components/homepage/navbar.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import logoCred from "./image/credLogo.png"
+import "./navbar.css"
+
+export function Navbar() {
+    return (
+        <div className="NavbarDiv">
+            <div className="firstHalf">
+                <div className="LogoDiv">
+                    <img src={logoCred} />
+                </div>
+                <div className="Navcontent">
+                    <div class="dropdown">
+                        <span>Products</span>
+                        <div class="dropdown-content">
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <span>Pricing</span>
+                        <div class="dropdown-content">
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <span>Solution</span>
+                        <div class="dropdown-content">
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <span>Customers</span>
+                        <div class="dropdown-content">
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <span>Resources</span>
+                        <div class="dropdown-content">
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <span>Partners</span>
+                        <div class="dropdown-content">
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="secondHalf" >
+                <div>
+                    <div class="dropdown">
+                        <span>English</span>
+                        <div class="dropdown-content">
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <span>Login</span>
+                    </div>
+                    <div class="dropdown">
+                        <button>Get a demo</button>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+}
