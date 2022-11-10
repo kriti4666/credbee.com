15bbe9b534b593225132ed4aa161d4ba43dced31
msnimish <msnimish24@gmail.com>
2022-11-10 19:07:23 +0530

    Installed styled-breakpoints

--- credbee/src/Components/Pricing/Pricing.jsx
+++ credbee/src/Components/Pricing/Pricing.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { HeroWrapper } from "./Pricing.styles";
+import { Tab, Tabs } from "./Tabs/Tabs";
+import { PlanPricingData } from "./PlanPricingData";
 
-const Pricing = () => {
+let data = PlanPricingData;
+
+const PricingPage = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <div>
+      {/* Navbar */}
+      <HeroWrapper>
+        <div className="heroContainer">
+          <div className="heroSection">
+            <h1 className="header">Find a plan that's right for you</h1>
+            <div className="subHeader">
+              <div>
+                Or simply leverage the expertise of our consultation team.
+              </div>
+              <div className="btnTalk">
+                <p>TALK TO US</p>
+                <BsArrowRight style={{ width: "12px" }} />
+              </div>
+            </div>
+            <div className="priceOptionBtns">
+              <Tabs className="periodBtns">
+                <Tab active={true}>Annual</Tab>
+                <Tab active={false}>Monthly</Tab>
+              </Tabs>
+              <Tabs className="currencyBtns">
+                <Tab active={true}>USD</Tab>
+                <Tab active={false}>EUR</Tab>
+                <Tab active={false}>GBP</Tab>
+                <Tab active={false}>AUD</Tab>
+                <Tab active={false}>CAD</Tab>
+                <Tab active={false}>INR</Tab>
+              </Tabs>
+            </div>
+            <div className="planSummaryContainer">
+              {data.map((e)=>(
+                <div className="planSummaryCard">
+                  <h1 className="planSumName">{e.name}</h1>
+                  <h3 className="planSumDesc">{e.description}</h3>
+                </div>
+              ))}
+              
+            </div>
+          </div>
+        </div>
+      </HeroWrapper>
+    </div>
+  );
+};
 
-export default Pricing
+export default PricingPage;
