8e7f8e3708ed652512f5004c1536f5dd36bcfc44
Gunjan Kuthe <gkuthe1997@gmail.com>
2022-11-10 22:18:38 +0530

    added day-3

--- credbee/src/Components/AllRoutes.jsx
+++ credbee/src/Components/AllRoutes.jsx
@@ -3,24 +3,32 @@ import { Route, Routes } from 'react-router-dom'
 import Admin from './Admin/Admin'
 import Customers from './Customers/Customers'
 import Demo from './Demo/Demo'
-import Navbar from './Homepage/Navbar'
+import Navbar from './homepage/Navbar'
 import Login from './LoginSignup/Login'
 import Partners from './Partners/Partners'
 import Pricing from './Pricing/Pricing'
+import {ChakraProvider} from "@chakra-ui/react"
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navbar/>}/>
-      <Route path='/login' element={<Login/>}/>
+      <Route path="/" element={<Navbar />} />
+      <Route path="/login" element={<Login />} />
       {/* <Route path='/signup' element={<SignUp/>}/> */}
-      <Route path='/pricing' element={<Pricing/>}/>
-      <Route path='/demo' element={<Demo/>}/>
-      <Route path='/customers' element={<Customers/>}/>
-      <Route path='/partner' element={<Partners/>}/>
-      <Route path='/admin' element={<Admin/>}/>
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/demo" element={<Demo />} />
+      <Route path="/customers" element={<Customers />} />
+      <Route path="/partner" element={<Partners />} />
+      <Route
+        path="/admin"
+        element={
+          <ChakraProvider>
+            <Admin />
+          </ChakraProvider>
+        }
+      />
     </Routes>
-  )
+  );
 }
 
 export default AllRoutes
