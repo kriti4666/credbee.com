aa71e64c95f958c90116b7a0818ad93d94cd9c83
msnimish <msnimish24@gmail.com>
2022-11-10 09:49:44 +0530

    Pricing Data Updated

--- credbee/src/Components/Pricing/PlanPricingData.jsx
+++ credbee/src/Components/Pricing/PlanPricingData.jsx
@@ -1,122 +1,126 @@
 export const PlanPricingData = [
       {
-        "name": "Launch",
-        "description": "For early-stage startups that want to spend more time developing and less on manual operations.",
-        "currency": "USD",
-        "amount": "0",
-        "billingPeriod": "billed annually",
-        "benefits": "for your first USD 100k in revenue What happens when I hit USD 100k?",
-        "btnTitle": "Get started for free",
-        "btnTitle_url": "https://www.chargebee.com/trial-signup/?ref=Launch",
-        "featuresTitle": "All you need to get started",
-        "featureDetails": [
-          {
-            "featureName": "Secure checkout (.js and hosted)"
-          },
-          {
-            "featureName": "Support up to 6 pricing models"
-          },
-          {
-            "featureName": "Issue credit notes"
-          },
-          {
-            "featureName": "Dunning"
-          },
-          {
-            "featureName": "A/R aging report"
-          },
-          {
-            "featureName": "Customer portal"
-          }
-        ]
+    name: "Launch",
+    description:
+      "For early-stage startups that want to spend more time developing and less on manual operations.",
+    currency: "USD",
+    amount: "0",
+    billingPeriod: "billed annually",
+    benefits:
+      "for your first USD 100k in revenue What happens when I hit USD 100k?",
+    btnTitle: "Get started for free",
+    btnTitle_url: "https://www.chargebee.com/trial-signup/?ref=Launch",
+    featuresTitle: "All you need to get started",
+    featureDetails: [
+      {
+        featureName: "Secure checkout (.js and hosted)",
       },
       {
-        "name": "Rise",
-        "description": "For agile startups that want to grow their revenue with quick experiments and data-driven decision making.",
-        "currency": "USD",
-        "amount": "249",
-        "billingPeriod": "billed annually",
-        "benefits": "includes USD 600K/yr revenue\n0.6% of overage revenue",
-        "btnTitle": "Schedule a demo",
-        "btnTitle_url": "https://www.chargebee.com/schedule-a-demo/?ref=Rise",
-        "featuresTitle": "Everything in Launch +",
-        "featureDetails": [
-          {
-            "featureName": "Custom domain"
-          },
-          {
-            "featureName": "Multi-language support"
-          },
-          {
-            "featureName": "Sales tax automation"
-          },
-          {
-            "featureName": "Advanced analytics"
-          },
-          {
-            "featureName": "CRM integration"
-          },
-          {
-            "featureName": "Accounting integration"
-          }
-        ]
+        featureName: "Support up to 6 pricing models",
       },
       {
-        "name": "Scale",
-        "description": "For fast-growth scaleups that want to grow by maximizing efficiencies in their revenue operations.",
-        "currency": "USD",
-        "amount": "549",
-        "billingPeriod": "billed annually",
-        "benefits": "includes USD 1.2M/yr revenue 0.75% of overage revenue",
-        "btnTitle": "Schedule a demo",
-        "btnTitle_url": "https://www.chargebee.com/schedule-a-demo/?ref=Scale",
-        "featuresTitle": "Everything in Rise +",
-        "featureDetails": [
-          {
-            "featureName": "User role management"
-          },
-          {
-            "featureName": "Multiple payment methods"
-          },
-          {
-            "featureName": "Advance invoices"
-          },
-          {
-            "featureName": "Multiple tax profiles"
-          },
-          {
-            "featureName": "Smart dunning"
-          }
-        ]
+        featureName: "Issue credit notes",
       },
       {
-        "name": "Enterprise",
-        "description": "For large businesses looking for enterprise-class compliance while diversifying revenue streams.",
-        "amount": "Custom",
-        "benefits": "Get a quote tailored to your requirements.",
-        "btnTitle": "",
-        "btnTitle_url": "https://www.chargebee.com/schedule-a-demo/?ref=Enterprise",
-        "featuresTitle": "Everything in Scale +",
-        "featureDetails": [
-          {
-            "featureName": "Contract terms"
-          },
-          {
-            "featureName": "Account hierarchy"
-          },
-          {
-            "featureName": "Quotes"
-          },
-          {
-            "featureName": "Chargeback automation"
-          },
-          {
-            "featureName": "Dedicated CSM"
-          },
-          {
-            "featureName": "Priority phone support"
-          }
-        ]
-      }
-    ]
-  
+        featureName: "Dunning",
+      },
+      {
+        featureName: "A/R aging report",
+      },
+      {
+        featureName: "Customer portal",
+      },
+    ],
+  },
+  {
+    name: "Rise",
+    description:
+      "For agile startups that want to grow their revenue with quick experiments and data-driven decision making.",
+    currency: "USD",
+    amount: "249",
+    billingPeriod: "billed annually",
+    benefits: "includes USD 600K/yr revenue\n0.6% of overage revenue",
+    btnTitle: "Schedule a demo",
+    btnTitle_url: "https://www.chargebee.com/schedule-a-demo/?ref=Rise",
+    featuresTitle: "Everything in Launch +",
+    featureDetails: [
+      {
+        featureName: "Custom domain",
+      },
+      {
+        featureName: "Multi-language support",
+      },
+      {
+        featureName: "Sales tax automation",
+      },
+      {
+        featureName: "Advanced analytics",
+      },
+      {
+        featureName: "CRM integration",
+      },
+      {
+        featureName: "Accounting integration",
+      },
+    ],
+  },
+  {
+    name: "Scale",
+    description:
+      "For fast-growth scaleups that want to grow by maximizing efficiencies in their revenue operations.",
+    currency: "USD",
+    amount: "549",
+    billingPeriod: "billed annually",
+    benefits: "includes USD 1.2M/yr revenue 0.75% of overage revenue",
+    btnTitle: "Schedule a demo",
+    btnTitle_url: "https://www.chargebee.com/schedule-a-demo/?ref=Scale",
+    featuresTitle: "Everything in Rise +",
+    featureDetails: [
+      {
+        featureName: "User role management",
+      },
+      {
+        featureName: "Multiple payment methods",
+      },
+      {
+        featureName: "Advance invoices",
+      },
+      {
+        featureName: "Multiple tax profiles",
+      },
+      {
+        featureName: "Smart dunning",
+      },
+    ],
+  },
+  {
+    name: "Enterprise",
+    description:
+      "For large businesses looking for enterprise-class compliance while diversifying revenue streams.",
+    amount: "Custom",
+    benefits: "Get a quote tailored to your requirements.",
+    btnTitle: "",
+    btnTitle_url: "https://www.chargebee.com/schedule-a-demo/?ref=Enterprise",
+    featuresTitle: "Everything in Scale +",
+    featureDetails: [
+      {
+        featureName: "Contract terms",
+      },
+      {
+        featureName: "Account hierarchy",
+      },
+      {
+        featureName: "Quotes",
+      },
+      {
+        featureName: "Chargeback automation",
+      },
+      {
+        featureName: "Dedicated CSM",
+      },
+      {
+        featureName: "Priority phone support",
+      },
+    ],
+  },
+];
