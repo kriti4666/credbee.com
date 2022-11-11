f4081604e10765efed95942542876f1f8d93c3d5
arnab9593 <arnabpal679@gmail.com>
2022-11-11 13:55:39 +0530

    day 4 release 1

---
+++ Backend/Schemas/user.schema.js
@@ -0,0 +1,26 @@
+
+import mongoose from "mongoose";
+
+//schema validation i.e. form data validation 
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+    phone: {
+        type: Number,
+        required: true,
+        trim: true,
+        unique: true,
+    }
+}, {
+    versionKey: false,
+    timestamps: true,
+})
+
+const User = mongoose.model("user", userSchema)
+
+export default User
---
+++ credbee/src/Components/API/signupAPI.js
@@ -0,0 +1,14 @@
+
+import axios from "axios"
+
+//server URL
+const URL = 'http://localhost:8080'
+
+export const authenticateSignup = async (data) => {
+    try {
+        //server url and data for post 
+        return await axios.post(`${URL}/signup`, data)
+    } catch (e) {
+        console.log(e.message);
+    }
+}
